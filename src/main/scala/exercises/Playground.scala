6ae13d20a3476263fb1c2c535ec738a40a3f263a
Gena <gehazzz@gmail.com>
2021-12-04 14:54:05 +0200

    cluster setup

--- src/main/scala/exercises/Playground.scala
+++ src/main/scala/exercises/Playground.scala
@@ -1,10 +1,23 @@
 package exercises
 
+import org.apache.spark.sql.SparkSession
+
 object Playground extends App {
 
-  val spark = ExerciseUtils.getSparkSession("Postgres Spark Playground App")
+  /*val spark = ExerciseUtils.getSparkSession("Postgres Spark Playground App")
 
   var df2 = spark.read.parquet("D:/temp/ITRACKQ_GL_TRNS_TIGQ_20211114-094534421.parquet.snappy")
-  df2.show(false)
+  df2.show(false)*/
+
+  val spark = SparkSession.builder()
+    .appName("Postgres Spark Playground App")
+    .config("spark.master", "spark://localhost:7077")
+    .getOrCreate()
+
+  import spark.implicits._
+  val columns = Seq("language","users_count")
+  val data = Seq(("Java", "20000"), ("Python", "100000"), ("Scala", "3000"))
+
+  data.toDF().show()
 
 }
