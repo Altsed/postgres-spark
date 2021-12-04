20c84e667daf6ee3bd214836f47f731009de2ac5
Gena <gehazzz@gmail.com>
2021-12-04 18:31:23 +0200

    - postgres docker compose added - init sql added - code organisation example added

---
+++ src/main/scala/exercises/basic/RetrieveEverythingFromTable.scala
@@ -0,0 +1,22 @@
+package exercises.basic
+
+import org.apache.spark.sql.SparkSession
+
+object RetrieveEverythingFromTable extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Spark Basic Sql Practice")
+    .config("spark.master", "local[*]")
+    .getOrCreate()
+
+  val facilitiesDf = spark.read
+    .format("jdbc")
+    .option("driver", "org.postgresql.Driver")
+    .option("url", "jdbc:postgresql://localhost:5432/exercises")
+    .option("dbtable", "cd.facilities")
+    .option("user", "spark")
+    .option("password", "spark")
+    .load()
+
+  facilitiesDf.show(100, false)
+}
